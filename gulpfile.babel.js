eb7f0fe93ec21fb7de725409b46c410574306fb1
NoMoreBuild <rainha1005@gmail.com>
2020-12-27 14:49:19 +0900

    feature: 개발환경 추가, 빌드 추가, 배포기능 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,85 @@
 import gulp from "gulp";
-import pug from "gulp-pug";
+import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import sass from "gulp-sass";
+import autop from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 const routes = {
   pug: {
-    src: "src/**/*.pug",
+    watch: "src/**/*.pug",
+    src: "src/*.pug",
     dest: "build",
   },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js",
+  },
+};
+const clean = () => del(["build"]);
+
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const pug = () =>
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autop())
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+
+const js = async () => {
+  await gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
 };
 
-export const pug = () => {
-  gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest(routes.pug.src.dest));
+const gh = () => gulp.src("build/**/*").pipe(ghPages());
+
+const webserver = () => {
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
 };
 
-export const dev = () => console.log("I will dev");
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
+};
+
+const prepare = gulp.series([clean, img]);
+
+const asstes = gulp.series([pug, styles, js]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const build = gulp.series([prepare, asstes]);
+export const dev = gulp.series([build, postDev]);
+export const deploy = gulp.series([build, gh]);
